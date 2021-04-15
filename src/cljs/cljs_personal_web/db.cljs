6ca094b72e856e2968f824a8abc9a982131f1330
Evan Jones <evanpeterjones@gmail.com>
2021-04-15 17:53:19 -0500

    updated the xml parser to pull the nth key, it's a multi-arity function so by default it should work the exact same as before just keeping both for funsies---also for testing stuff.

--- src/cljs/cljs_personal_web/db.cljs
+++ src/cljs/cljs_personal_web/db.cljs
@@ -15,22 +15,45 @@
   (GET "addPodcast" {:params {:url url}
                      :handler #(swap! state assoc :podcast  %)}))
 
-(def get-key-content-from-result
+(def get-nth-key-content
+  (fn
+    ([k r]
+     "recursive depth-first search on xml tree to find the content of the nth occurence of a given key"
+     (get-nth-key-content k r 1 1))
+    ([#^Keyword k
+      #^PersistentArrayMap r
+      #^Number n]
+     (get-nth-key-content k r 1 n))
+    ([k r i n]
+     (if (= cljs.core/PersistentVector (type r))
+       (let [res (get-nth-key-content k (first r) i n)]
+         (if res res (get-nth-key-content k (rest r) i n)))
+       (when (:tag r)
+         (if (= k (:tag r))
+           ;; FOUND IT: in this case, since we've found it we only return the content if our count is the same
+           ;; Otherwise we want to call the search again with an incremented count
+           (if (= i n)
+             (:content r)
+             (get-nth-key-content k (:content r) (inc i) n))
+
+           (get-nth-key-content k (:content r) i n)))))))
+
+(def get-key-content
   (fn [k r]
     "recursive depth-first search on xml tree to find the content of the given key"
-    (if (= cljs.core/PersistentVector (type r))
-      (let [res (get-key-content-from-result k (first r))]
+    (if (= cljs.core/List (type r))
+      (let [res (get-key-content k (first r))]
         (if res res
-          (get-key-content-from-result k (rest r))))
+                (get-key-content k (rest r))))
       (when (:tag r)
         (if (= k (:tag r))
           (:content r)
-          (get-key-content-from-result k (:content r)))))))
+          (get-key-content k (:content r)))))))
 
 (def get-title-from-result
   (fn [r]
-    {:link (get-key-content-from-result :link r)
-     :name (get-key-content-from-result :title r)}))
+    {:link (get-key-content :link r)
+     :name (get-key-content :title r)}))
 
 (defn get-feed! [url state]
   (GET "/getRssData" {:params  {:url url}

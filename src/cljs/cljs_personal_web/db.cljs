e7bc379c4a7a33ed243e6293c284341d42215f00
Evan Jones <evanpeterjones@gmail.com>
2021-04-15 18:10:24 -0500

    fixed an issue with the vectors so it's working in all cases, but it's in a way I'm not happy with bc it isn't super clear and it also helped me create a stackoverflow when I'm trying to find anything but the first instance which means for sure I'm not reaching a base case somehow bc that doesn't make any sense at all. I think it's because something is 'technically' not a map and so we're constantly recuring because that one case. This is why there has to be some way for be to distinguish 'is-map' from 'is-list' but some lists aren't actually lists and that makes me want to kill myself.

--- src/cljs/cljs_personal_web/db.cljs
+++ src/cljs/cljs_personal_web/db.cljs
@@ -25,18 +25,20 @@
       #^Number n]
      (get-nth-key-content k r 1 n))
     ([k r i n]
-     (if (= cljs.core/PersistentVector (type r))
-       (let [res (get-nth-key-content k (first r) i n)]
-         (if res res (get-nth-key-content k (rest r) i n)))
+     (if (not (map? r))
+       #_(or (= cljs.core/PersistentVector (type r))        ;; idk why clojure doesn't let me say (is-sequable? r)
+           (= cljs.core/List (type r)))                     ;; of (is-firstable? r) would be super convenient too, lol
+       (let [res (recur k (first r) i n)]
+         (if res res (recur k (rest r) i n)))
        (when (:tag r)
          (if (= k (:tag r))
            ;; FOUND IT: in this case, since we've found it we only return the content if our count is the same
            ;; Otherwise we want to call the search again with an incremented count
            (if (= i n)
              (:content r)
-             (get-nth-key-content k (:content r) (inc i) n))
+             (recur k (:content r) (inc i) n))
 
-           (get-nth-key-content k (:content r) i n)))))))
+           (recur k (:content r) i n)))))))
 
 (def get-key-content
   (fn [k r]

4f926c330727b6e6393f1022dba35a41514587c6
Evan Jones <evanpeterjones@gmail.com>
2021-05-26 22:25:05 -0500

    started updating the icons, css is still funky

--- src/cljs/cljs_personal_web/components/audio_player.cljs
+++ src/cljs/cljs_personal_web/components/audio_player.cljs
@@ -4,8 +4,7 @@
 ;; by default should just display a pause/play button
 
 (def player
-  (let [                                                    ;paused? (-> js/document (.getElementById "player") .-paused)
-        toggle-play (fn [_]
+  (let [toggle-play (fn [_]
                       (let [p (-> js/document (.getElementById "player"))]
                         (if (.-paused p)
                           (.play p)
@@ -14,6 +13,6 @@
       (when link
         [:div.player
          [:button.pause {:on-click toggle-play} "Pause/Play"]
-         [:audio#player {:controls " " :auto-play " " :preload "auto"}
+         [:audio#player {:controls " " :preload "auto"}
           [:source {:src link :type "audio/mpeg"}]
           "Your browser does not support playing this type of audio file."]]))))
--- src/cljs/cljs_personal_web/podcast_feed.cljs
+++ src/cljs/cljs_personal_web/podcast_feed.cljs
@@ -14,26 +14,40 @@
                                  :name "Random Number Generator Horror Podcast No.9"}]})]
     (fn []
       [:div
-       [:div#hero
-        [:div.container
-         (when (:episodes @state)
-           [:div.container
-            [:div#work
-             [:h1 "Episodes"]
+       (when (:episodes @state)
+         [:div#episodes
+          [:div.container
+           [:ul [:li [:h2 "Episodes"]]]
 
-             [:div.scrollable-vertical.scrollable-sm
-              [:ul (map episode (:episodes @state))]]]])]]
+           [:div.scrollable-vertical.scrollable-sm
+            [:ul (map episode (:episodes @state))]]]])
+
+       [:div#podcasts.scrollmenu
+        [:img {:src "https://cdn.onlinewebfonts.com/svg/img_433041.png"
+               :alt "Podcast Cover Art"}]
+        [:img {:src "https://cdn.onlinewebfonts.com/svg/img_433041.png"
+               :alt "Podcast Cover Art"}]
+        [:img {:src "https://cdn.onlinewebfonts.com/svg/img_433041.png"
+               :alt "Podcast Cover Art"}]
+        [:img {:src "https://cdn.onlinewebfonts.com/svg/img_433041.png"
+               :alt "Podcast Cover Art"}]]
 
        [:div.container
         [:div#work
          [:div
           ;[:p {:class "new" :style {:font-size "1.5em"}} "+"]
-          [:ul [:li [:h2 "Podcasts"]]]]
+          [:br]
+          [:h2 "Podcasts"]
+          [:br]]
 
-         [:div.podcasts
-          ;gotta have some horizontal css
-          ;gotta have some images here
-          ]
+         [:div.left
+          [:span.border-blue
+           [:div#podcasts.scrollmenu
+            (for [li-link (:titles @state)]
+              ^{:key li-link}
+              [:img
+               {:src "https://cdn.onlinewebfonts.com/svg/img_433041.png"
+                :alt "Podcast Cover Art"}])]]]
 
          [links "left" "border-blue" (:titles @state) (fn [x]
                                                         (js/console.log x)
